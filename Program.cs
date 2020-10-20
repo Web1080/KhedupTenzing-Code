66119ed76e47d6d0380fcb8f3c6ee91638521533
Web1080 <kheduptenzing665@gmail.com>
2020-10-20 09:01:20 -0500

    Hello World

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//1.0 predefined namespace
+
+using System;
+
+//1.1 custom defined namespace
+namespace helloWorld
+{
+    class Program
+    {
+        //execution starts here
+                //Commandline arguments
+                // vvv
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World! " +DateTime.Now);
+            string firstname, lastname;
+            Console.WriteLine("Please Enter Your First Name: ");
+            firstname = Console.ReadLine(); //Takes user's input in string form
+            //Console.WriteLine("Welcome " +firstname + " " +lastname); //String concatation
+            Console.WriteLine("Please Enter Your Last Name: ");
+            lastname = Console.ReadLine();
+            Console.WriteLine($"Welcome {firstname} {lastname}"); //String interpolation
+        }
+    }
+}
